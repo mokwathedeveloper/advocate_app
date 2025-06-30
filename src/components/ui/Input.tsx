ab614a15e0cc1373cbcb5bc531c5899bdc73c4f3
mokwathedeveloper <moffatmokwa12@gmail.com>
2025-06-29 18:54:53 -0700

    Update src/components/ui/Input.tsx - Fix React ref warning by implementing forwardRef for form compatibility

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -9,14 +9,14 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode;
 }
 
-const Input: React.FC<InputProps> = ({
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({
   label,
   error,
   helperText,
   icon,
   className,
   ...props
-}) => {
+}, ref) => {
   const baseClasses = "w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-navy-500 focus:border-transparent transition-colors";
   const errorClasses = error ? "border-red-500" : "border-gray-300";
   const iconClasses = icon ? "pl-10" : "";
@@ -37,6 +37,7 @@ const Input: React.FC<InputProps> = ({
           </div>
         )}
         <input
+          ref={ref}
           className={classes}
           {...props}
         />
@@ -49,6 +50,8 @@ const Input: React.FC<InputProps> = ({
       )}
     </div>
   );
-};
+});
+
+Input.displayName = 'Input';
 
 export default Input;
